f3a9286cddb9d05f721d4f61a6483f4494cc3f07
Emil Redzik <emilredzik1994@gmail.com>
2021-04-24 12:47:14 +0200

    New layout 2

--- scripts/build_frontend.sh
+++ scripts/build_frontend.sh
@@ -3,5 +3,5 @@ js="../static/main.js"
 sass "style.sass" "../static/style.css"
 #--optimize
 elm make src/Main.elm --output=$js --optimize #--debug
-minimized="../static/main.min.js"
-uglifyjs $js -o $minimized
+# minimized="../static/main.min.js"
+# uglifyjs $js -o $minimized
