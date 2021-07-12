a7238298f7888da7d4331550d9805d9261acd7f2
Emil Redzik <emilredzik1994@gmail.com>
2021-07-13 00:50:41 +0200

    Fixed netlify build

--- frontend/scripts/build.sh
+++ frontend/scripts/build.sh
@@ -5,6 +5,7 @@ then
     export API_URL="http://localhost:1234/api/openapi.json";
     echo "Using default dev api path: ${API_URL}"
 else
+    export API_URL="$API_URL/openapi.json"
     echo "Using set api path: ${API_URL}"
 fi
 
