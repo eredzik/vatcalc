e22ddb86afabc0e585c6b608da169169b94eb7e8
Emil Redzik <emilredzik1994@gmail.com>
2021-07-12 17:53:20 +0200

    Fixed api path for test build

--- frontend/scripts/build.sh
+++ frontend/scripts/build.sh
@@ -5,6 +5,7 @@ else export API_PATH="${URL}/api/openapi.json"; fi
 rm -rf .elm-spa;
 npx elm-spa gen;
 rm -rf .generated-api;
+echo "Api path: ${API_PATH}"
 npx openapi-generator-cli generate -i ${API_PATH} -g elm -o .generated-api
 mv .generated-api/src/* .elm-spa/generated;
 rm -rf .generated-api;
