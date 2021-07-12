3b8103166010f89ca0f337e21ffca415110c5c97
Emil Redzik <emilredzik1994@gmail.com>
2021-07-12 18:07:46 +0200

    Fixed build frontend

--- frontend/scripts/build.sh
+++ frontend/scripts/build.sh
@@ -1,22 +1,22 @@
 npm install
 
-if [ -z ${URL}];
+if [ -z "${URL}"];
 then
-    if [-z ${API_URL}];
+    if [ -z "${API_URL}"];
     then
-        export API_PATH="http://localhost:1234/api/openapi.json";
-        echo "Using default dev api path: ${API_PATH}"
+        export API_URL="http://localhost:1234/api/openapi.json";
+        echo "Using default dev api path: ${API_URL}"
     else
-        echo "Using set api path: ${API_PATH}"
+        echo "Using set api path: ${API_URL}"
     fi
 else
-export API_PATH="${URL}/api/openapi.json";
-echo "Using deployment api path: ${API_PATH}"
+export API_URL="${URL}/api/openapi.json";
+echo "Using deployment api path: ${API_URL}"
 fi
 rm -rf .elm-spa;
 npx elm-spa gen;
 rm -rf .generated-api;
-echo "Api path: ${API_URL}"
+echo "Api path: ${API_URL}";
 npx openapi-generator-cli generate -i ${API_URL} -g elm -o .generated-api
 mv .generated-api/src/* .elm-spa/generated;
 rm -rf .generated-api;
